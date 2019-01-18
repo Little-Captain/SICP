d0a53e5072894e83994c7d6e0e36d715fb0ff7fb
Little-Captain <coder.littlecaptain@gmail.com>
2019-01-18 14:19:43 +0800

    1.1学习完成

--- 1.1.8.rkt
+++ 1.1.8.rkt
@@ -1,1 +1,31 @@
-#lang racket
+#lang sicp
+(define (square x) (* x x))
+(define (square1 x)
+  (exp (double (log x))))
+(define (double x) (+ x x))
+
+; 内部定义和块结构
+;(define (sqrt x)
+;  (define (good-enough? guess x)
+;    (< (abs (- (square guess) x)) 0.001))
+;  (define (average x y)
+;    (/ (+ x y) 2))
+;  (define (improve guess x)
+;    (average guess (/ x guess)))
+;  (define (sqrt-iter guess x)
+;    (if (good-enough? guess x)
+;        guess
+;        (sqrt-iter (improve guess x) x)))
+;  (sqrt-iter 1.0 x))
+(define (sqrt x)
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (average n1 n2)
+    (/ (+ n1 n2) 2))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess)
+        guess
+        (sqrt-iter (improve guess))))
+  (sqrt-iter 1.0))
